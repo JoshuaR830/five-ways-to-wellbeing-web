92b3ac181c1b897d588a1b3a94bc77484ee1a803
Joshua <joshuaquarryhouse@gmail.com>
2021-06-29 11:51:15 +0100

    Added initial project

---
+++ five-ways-to-wellbeing-web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace five_ways_to_wellbeing_web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
