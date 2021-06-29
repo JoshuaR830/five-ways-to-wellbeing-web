868b4563a23c55950bd978eb6a64a987ae9fe34a
Joshua <joshuaquarryhouse@gmail.com>
2021-06-29 16:11:13 +0100

    Added a graph

---
+++ five-ways-to-wellbeing-web/wwwroot/js/colors.js
@@ -0,0 +1,5 @@
+﻿const way_to_wellbeing_be_active = "#f693a7";
+const way_to_wellbeing_connect = "#ffd176";
+const way_to_wellbeing_give = "#79dc9d";
+const way_to_wellbeing_keep_learning = "#6ac8df";
+const way_to_wellbeing_take_notice = "#c994e9";
--- five-ways-to-wellbeing-web/wwwroot/js/site.js
+++ five-ways-to-wellbeing-web/wwwroot/js/site.js
@@ -1,4 +1,38 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿const centerX = 100;
+const centerY = 100;
 
-// Write your JavaScript code.
+window.addEventListener("load", function() {
+    let c = document.getElementById("graph-canvas");
+    let graph = c.getContext("2d");
+    
+    let numberOfArcs = 5;
+    
+    calculateArc(graph, numberOfArcs, 0, 100, way_to_wellbeing_connect)
+    calculateArc(graph, numberOfArcs, 1, 10, way_to_wellbeing_be_active)
+    calculateArc(graph, numberOfArcs, 2, 100, way_to_wellbeing_keep_learning)
+    calculateArc(graph, numberOfArcs, 3, 40, way_to_wellbeing_take_notice)
+    calculateArc(graph, numberOfArcs, 4, 100, way_to_wellbeing_give)
+})
+
+function calculateArc(graph, numberOfArcs, arcNumber, radius, fill) {
+    
+    // 1/4 circle (2PI) + half an arc offset
+    let arcOffset = 0.5 + 1/numberOfArcs;
+    
+    // Positions for start and end of the arc
+    let arcStart = (arcNumber * (2/numberOfArcs)) - arcOffset;
+    let arcEnd = ((arcNumber + 1) * (2/numberOfArcs)) - arcOffset;
+    
+    drawArc(graph, arcStart, arcEnd, radius, fill)
+}
+
+function drawArc(graph, arcStart, arcEnd, radius, fill) {
+    graph.beginPath();
+    console.log(centerX)
+    console.log(centerY)
+    graph.moveTo(centerX, centerY);
+    graph.arc(centerX, centerY, radius, arcStart * Math.PI, arcEnd * Math.PI)
+    graph.fillStyle = fill
+    graph.fill();
+    graph.closePath();
+}
